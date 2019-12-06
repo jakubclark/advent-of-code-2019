18abdd6422b8713f892f73a721aabd4f390f0746
Kuba Clark <jakub.clark@protonmail.com>
2019-12-06 01:23:07 +0100

    Day 2 - Part 2 - Partial/Ugly solution

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,5 +1,4 @@
 use std::fs::read_to_string;
-use std::io::Error as IoError;
 
 const ERR_MSG: &str = "No entry found";
 
@@ -13,19 +12,19 @@ fn string_to_vec(input: String) -> Vec<i64> {
         .collect()
 }
 
-fn get_parts(i: usize, nums: &[i64]) -> (i64, i64, usize) {
-    let left_index = nums.get(i + 1).expect(ERR_MSG);
-    let right_index = nums.get(i + 2).expect(ERR_MSG);
+fn get_parts(i: usize, nums: &[i64]) -> Result<(i64, i64, usize), ()> {
+    let left_index = nums.get(i + 1).ok_or(())?;
+    let right_index = nums.get(i + 2).ok_or(())?;
 
-    let left = nums.get(*left_index as usize).expect(ERR_MSG);
-    let right = nums.get(*right_index as usize).expect(ERR_MSG);
+    let left = nums.get(*left_index as usize).ok_or(())?;
+    let right = nums.get(*right_index as usize).ok_or(())?;
 
-    let dest = nums.get(i + 3).expect(ERR_MSG);
+    let dest = nums.get(i + 3).ok_or(())?;
 
-    (*left, *right, *dest as usize)
+    Ok((*left, *right, *dest as usize))
 }
 
-fn run_program(mut nums: Vec<i64>) -> Vec<i64> {
+fn run_program(mut nums: Vec<i64>) -> Result<Vec<i64>, ()> {
     let mut i = 0;
 
     while let Some(opcode) = nums.get(i) {
@@ -33,7 +32,7 @@ fn run_program(mut nums: Vec<i64>) -> Vec<i64> {
             break;
         }
 
-        let (left, right, dest) = get_parts(i, &nums);
+        let (left, right, dest) = get_parts(i, &nums)?;
 
         let res = match *opcode {
             1 => left + right,
@@ -44,25 +43,96 @@ fn run_program(mut nums: Vec<i64>) -> Vec<i64> {
         *entry = res;
         i += 4;
     }
-    nums
+    Ok(nums)
 }
 
-fn part1() -> Result<(), IoError> {
-    let input_string = read_to_string("input.txt")?;
-    let mut nums = string_to_vec(input_string);
+fn run_parameterized_program(mut nums: Vec<i64>, noun: i64, verb: i64) -> Result<Vec<i64>, ()> {
+    let second = nums.get_mut(1).expect("");
+    *second = noun;
+    let third = nums.get_mut(2).expect("");
+    *third = verb;
+    run_program(nums)
+}
 
-    let second = nums.get_mut(1).expect(ERR_MSG);
-    *second = 12;
-    let third = nums.get_mut(2).expect(ERR_MSG);
-    *third = 2;
-
-    let res = run_program(nums);
-    println!("{:?}", res.get(0).unwrap());
+fn part1() -> Result<(), ()> {
+    let input_string = read_to_string("input.txt").expect("");
+    let nums = string_to_vec(input_string);
+    let res = run_parameterized_program(nums, 12, 2)?;
+    println!("{:?}", res);
     Ok(())
 }
 
-fn main() -> Result<(), IoError> {
+fn part2() -> Result<(), ()> {
+    let input_string = read_to_string("input.txt").expect("");
+    let nums = string_to_vec(input_string);
+
+    // Increasing noun, increased result by 331776
+    // Increasing verb, increased result by 1
+    // Increasing both, increased result by 331777
+    // Expected output                      19690720
+    // Find the noun and verb using pseudo binary search
+
+    let expected_first_entry: i64 = 19690720;
+
+    let candidate_nouns: Vec<i64> = (0..100).collect();
+    let candidate_verbs: Vec<i64> = (0..100).collect();
+
+    let mut noun_i: i64 = 50;
+    let mut verb_i: i64 = 0;
+
+    let mut first_entry;
+    let mut diff = i64::max_value();
+
+//    let left = 0;
+//    let right = 99;
+//    let mid = left + (right + 1) / 2;
+
+    while diff.abs() > 331776 {
+        let clone = nums.clone();
+        println!("noun_i={}", noun_i);
+        if diff > 0 {
+            // ceil because it turned out to provide the correct solution
+            noun_i += ((100 - noun_i) as f64 / 2.0).ceil() as i64;
+        } else {
+            noun_i -= ((100 - noun_i) as f64 / 2.0).ceil() as i64;
+        }
+
+        let noun = *candidate_nouns.get(noun_i as usize).unwrap();
+
+        if let Ok(res) = run_parameterized_program(clone, noun, 0) {
+            first_entry = *res.get(0).unwrap();
+            diff = expected_first_entry - first_entry;
+        }
+    }
+
+    // We got the correct noun value
+    let noun = *candidate_nouns.get(noun_i as usize).unwrap();
+
+    while verb_i < 100 {
+        let verb = *candidate_verbs.get(verb_i as usize).unwrap();
+
+        let clone = nums.clone();
+
+        if let Ok(res) = run_parameterized_program(clone, noun, verb) {
+            first_entry = *res.get(0).unwrap();
+            diff = expected_first_entry - first_entry;
+            if diff == 0 {
+                break;
+            }
+        }
+
+        verb_i += 1;
+    }
+
+    println!("noun_i={}", noun_i);
+    println!("verb_i={}", verb_i);
+
+    Ok(())
+}
+
+fn main() -> Result<(), ()> {
     part1()?;
+    part2()?;
     Ok(())
 }
 
@@ -81,7 +151,7 @@ mod tests {
     fn test2() {
         let expected = vec![2, 0, 0, 0, 99];
         let nums = string_to_vec(String::from("1,0,0,0,99"));
-        let result = run_program(nums);
+        let result = run_program(nums).unwrap();
         assert_eq!(result, expected);
     }
 
@@ -89,7 +159,7 @@ mod tests {
     fn test3() {
         let expected = vec![2, 3, 0, 6, 99];
         let nums = string_to_vec(String::from("2,3,0,3,99"));
-        let result = run_program(nums);
+        let result = run_program(nums).unwrap();
         assert_eq!(result, expected);
     }
 
@@ -97,7 +167,7 @@ mod tests {
     fn test4() {
         let expected = vec![2, 4, 4, 5, 99, 9801];
         let nums = string_to_vec(String::from("2,4,4,5,99,0"));
-        let result = run_program(nums);
+        let result = run_program(nums).unwrap();
         assert_eq!(result, expected);
     }
 
@@ -105,7 +175,7 @@ mod tests {
     fn test5() {
         let expected = vec![30, 1, 1, 4, 2, 5, 6, 0, 99];
         let nums = string_to_vec(String::from("1,1,1,4,99,5,6,0,99"));
-        let result = run_program(nums);
+        let result = run_program(nums).unwrap();
         assert_eq!(result, expected);
     }
 }
