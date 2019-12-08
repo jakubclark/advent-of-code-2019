e35963acb687b4cabfd01399ba1d7b15337cfd80
Kuba Clark <jakub.clark@protonmail.com>
2019-12-08 21:53:14 +0100

    intcode-computer accepts multiple input arguments

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -4,13 +4,13 @@ use intcode_computer::run_program;
 
 fn part1() {
     let input_string = read_to_string("input.txt").expect("Failed to open input.txt");
-    let (_, output) = run_program(input_string, 1);
+    let (_, output) = run_program(input_string, &vec![1]);
     println!("Part 1 Program Output: {:?}", output);
 }
 
 fn part2() {
     let input_string = read_to_string("input.txt").expect("Failed to open input.txt");
-    let (_, output) = run_program(input_string, 5);
+    let (_, output) = run_program(input_string, &vec![5]);
     println!("Part 2 Program Output: {:?}", output);
 }
 
--- intcode-computer/src/lib.rs
+++ intcode-computer/src/lib.rs
@@ -54,7 +54,7 @@ fn get_argument(
     }
 }
 
-pub fn run_program(program: String, input: i64) -> (Vec<i64>, Vec<i64>) {
+pub fn run_program(program: String, input: &[i64]) -> (Vec<i64>, Vec<i64>) {
     let mut memory: Vec<i64> = program
         .trim()
         .split(',')
@@ -65,6 +65,7 @@ pub fn run_program(program: String, input: i64) -> (Vec<i64>, Vec<i64>) {
 
     let mut i = 0;
     let mut output = vec![];
+    let mut input_i = 0;
 
     while let Some(opcode) = memory.get(i) {
         let instruction = Instruction::new(*opcode);
@@ -87,8 +88,13 @@ pub fn run_program(program: String, input: i64) -> (Vec<i64>, Vec<i64>) {
             }
             3 => {
                 let dest = memory[i + 1] as usize;
+                let input = *input.get(input_i).unwrap_or_else(|| {
+                    panic!("Not enough inputs provided. At least {} inputs expected",
+                           input_i);
+                });
                 println!("IN  {} => {}", input, dest);
                 memory[dest] = input;
+                input_i += 1;
                 i += 2;
             }
             4 => {
@@ -146,7 +152,7 @@ mod tests {
     #[test]
     fn test1() {
         let program = String::from("3,0,4,0,99");
-        let (memory, output) = run_program(program, 50);
+        let (memory, output) = run_program(program, &vec![50]);
         assert_eq!(output, vec![50]);
         assert_eq!(memory, vec![50, 0, 4, 0, 99]);
     }
@@ -154,14 +160,14 @@ mod tests {
     #[test]
     fn test2() {
         let program = String::from("1101,100,-1,0,99");
-        let (memory, _) = run_program(program, 0);
+        let (memory, _) = run_program(program, &vec![0]);
         assert_eq!(memory, vec![99, 100, -1, 0, 99]);
     }
 
     #[test]
     fn test3() {
         let program = String::from("3,9,8,9,10,9,4,9,99,-1,8");
-        let (_, output) = run_program(program, 1);
+        let (_, output) = run_program(program, &vec![1]);
         assert_eq!(output, vec![0]);
     }
 
@@ -171,21 +177,21 @@ mod tests {
     #[test]
     fn test4() {
         let program = String::from("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
-        let (_, output) = run_program(program, 0);
+        let (_, output) = run_program(program, &vec![0]);
         assert_eq!(output, vec![0]);
     }
 
     #[test]
     fn test5() {
         let program = String::from("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
-        let (_, output) = run_program(program, 50);
+        let (_, output) = run_program(program, &vec![50]);
         assert_eq!(output, vec![1]);
     }
 
     #[test]
     fn test6() {
         let program = String::from("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
-        let (_, output) = run_program(program, -50);
+        let (_, output) = run_program(program, &vec![-50]);
         assert_eq!(output, vec![1]);
     }
 
@@ -193,42 +199,42 @@ mod tests {
     #[test]
     fn test_is_equal_to_8_version1() {
         let program = String::from("3,9,8,9,10,9,4,9,99,-1,8");
-        let (_, output) = run_program(program, -50);
+        let (_, output) = run_program(program, &vec![-50]);
         assert_eq!(output, vec![0]);
     }
 
     #[test]
     fn test_is_equal_to_8_version2() {
         let program = String::from("3,9,8,9,10,9,4,9,99,-1,8");
-        let (_, output) = run_program(program, 8);
+        let (_, output) = run_program(program, &vec![8]);
         assert_eq!(output, vec![1]);
     }
 
     #[test]
     fn test_is_equal_to_8_version3() {
         let program = String::from("3,9,8,9,10,9,4,9,99,-1,8");
-        let (_, output) = run_program(program, 50);
+        let (_, output) = run_program(program, &vec![50]);
         assert_eq!(output, vec![0]);
     }
 
     #[test]
     fn test_is_equal_to_8_version4() {
         let program = String::from("3,3,1108,-1,8,3,4,3,99");
-        let (_, output) = run_program(program, -50);
+        let (_, output) = run_program(program, &vec![-50]);
         assert_eq!(output, vec![0]);
     }
 
     #[test]
     fn test_is_equal_to_8_version5() {
         let program = String::from("3,3,1108,-1,8,3,4,3,99");
-        let (_, output) = run_program(program, 8);
+        let (_, output) = run_program(program, &vec![8]);
         assert_eq!(output, vec![1]);
     }
 
     #[test]
     fn test_is_equal_to_8_version6() {
         let program = String::from("3,3,1108,-1,8,3,4,3,99");
-        let (_, output) = run_program(program, 50);
+        let (_, output) = run_program(program, &vec![50]);
         assert_eq!(output, vec![0]);
     }
 
@@ -236,63 +242,63 @@ mod tests {
     #[test]
     fn test_is_less_than_8_version1() {
         let program = String::from("3,9,7,9,10,9,4,9,99,-1,8");
-        let (_, output) = run_program(program, -50);
+        let (_, output) = run_program(program, &vec![-50]);
         assert_eq!(output, vec![1]);
     }
 
     #[test]
     fn test_is_less_than_8_version2() {
         let program = String::from("3,9,7,9,10,9,4,9,99,-1,8");
-        let (_, output) = run_program(program, 8);
+        let (_, output) = run_program(program, &vec![8]);
         assert_eq!(output, vec![0]);
     }
 
     #[test]
     fn test_is_less_than_8_version3() {
         let program = String::from("3,9,7,9,10,9,4,9,99,-1,8");
-        let (_, output) = run_program(program, 50);
+        let (_, output) = run_program(program, &vec![50]);
         assert_eq!(output, vec![0]);
     }
 
     #[test]
     fn test_is_lass_than_8_version4() {
         let program = String::from("3,3,1107,-1,8,3,4,3,99");
-        let (_, output) = run_program(program, -50);
+        let (_, output) = run_program(program, &vec![-50]);
         assert_eq!(output, vec![1]);
     }
 
     #[test]
     fn test_is_lass_than_8_version5() {
         let program = String::from("3,3,1107,-1,8,3,4,3,99");
-        let (_, output) = run_program(program, 8);
+        let (_, output) = run_program(program, &vec![8]);
         assert_eq!(output, vec![0]);
     }
 
     #[test]
     fn test_is_lass_than_8_version6() {
         let program = String::from("3,3,1107,-1,8,3,4,3,99");
-        let (_, output) = run_program(program, 50);
+        let (_, output) = run_program(program, &vec![50]);
         assert_eq!(output, vec![0]);
     }
 
     #[test]
     fn test_complex1() {
         let program = String::from("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
-        let (_, output) = run_program(program, -50);
+        let (_, output) = run_program(program, &vec![-50]);
         assert_eq!(output, vec![999]);
     }
 
     #[test]
     fn test_complex2() {
         let program = String::from("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
-        let (_, output) = run_program(program, 8);
+        let (_, output) = run_program(program, &vec![8]);
         assert_eq!(output, vec![1000]);
     }
 
     #[test]
     fn test_complex3() {
         let program = String::from("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
-        let (_, output) = run_program(program, 50);
+        let (_, output) = run_program(program, &vec![50]);
         assert!(output[0] > 1000);
     }
 }
