83fb88a7277c122b66a75b30ea5bf6624229a14d
Kuba Clark <jakub.clark@protonmail.com>
2019-12-11 21:15:54 +0100

    Day 9 - Part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -3,6 +3,8 @@ use std::fs::read_to_string;
 
 fn main() {
     let program = read_to_string("input.txt").expect("Failed to open input.txt");
-    let (_, output) = run_program(program, &[1]);
-    println!("{:?}", output);
+    let (_, output1) = run_program(program.clone(), &[1]);
+    println!("Solution for part 1 = {:?}", output1[0]);
+    let (_, output2) = run_program(program, &[2]);
+    println!("Solution for part 2 = {:?}", output2[0]);
 }
